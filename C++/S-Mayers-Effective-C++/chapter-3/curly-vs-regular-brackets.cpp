2e9f39d34b49fc562ca020c1695e237b994d8773
Xelerezex <nikitavalderman@gmail.com>
2021-08-05 13:39:27 +0300

    C++. Scott MAyes. Chapter 3. Adding 3.1, 3.2

--- C++/S-Mayers-Effective-C++/chapter-3/curly-vs-regular-brackets.cpp
+++ C++/S-Mayers-Effective-C++/chapter-3/curly-vs-regular-brackets.cpp
@@ -1,5 +1,28 @@
 #include <iostream>
 
+
+class Widget {
+    public:
+        Widget(int i, bool b) {
+            std::cout << "First constructor" << std::endl;
+        }
+        Widget(int i, double d) {
+            std::cout << "Second constructor" << std::endl;
+        }
+        Widget(std::initializer_list<long double> il) {
+            std::cout << "Third constructor" << std::endl;
+        }
+
+};
+
+
 int main() {
+
+    Widget w1(10, true);
+    Widget w2{10, true};
+
+    Widget w3(10, 5.0);
+    Widget w4{10, 5.0};
+
     return 0;
 }
