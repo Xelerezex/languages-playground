eab0ecaf6d4b1002d18fd866235aff9570c73a88
Xelerezex <nikitavalderman@gmail.com>
2021-12-01 17:27:27 +0300

    C++. Art of C++. Red Belt. Week 5. Adding listings for video about lock_guard<mutex>.

---
+++ C++/Art-Of-Development-C++/Red Belt/week-5/18-no-execution.cpp
@@ -0,0 +1,140 @@
+#include "profile.h"
+
+#include <execution>    // <- ERROR
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+#include <future>
+#include <vector>
+using namespace std;
+
+template <typename Iterator>
+class IteratorRange {
+public:
+    IteratorRange(Iterator begin, Iterator end)
+        : first(begin)
+        , last(end)
+        , size_(distance(first, last))
+    {
+    }
+
+    Iterator begin() const {
+        return first;
+    }
+
+    Iterator end() const {
+        return last;
+    }
+
+    size_t size() const {
+        return size_;
+    }
+
+private:
+    Iterator first, last;
+    size_t size_;
+};
+
+template <typename Iterator>
+class Paginator
+{
+private:
+    vector<IteratorRange<Iterator>> pages;
+
+public:
+    Paginator(Iterator begin, Iterator end, size_t page_size)
+    {
+        for (size_t left = distance(begin, end); left > 0; )
+        {
+            size_t current_page_size = min(page_size, left);
+            Iterator current_page_end = next(begin, current_page_size);
+            pages.push_back({begin, current_page_end});
+
+            left -= current_page_size;
+            begin = current_page_end;
+        }
+    }
+
+    auto begin() const
+    {
+        return pages.begin();
+    }
+
+    auto end() const
+    {
+        return pages.end();
+    }
+
+    size_t size() const
+    {
+        return pages.size();
+    }
+};
+
+template <typename C>
+auto Paginate(C& c, size_t page_size)
+{
+    return Paginator(begin(c), end(c), page_size);
+}
+
+
+template <typename ContainerOfVectors>
+void GenerateSingleThread(
+            ContainerOfVectors &result,
+            size_t first_row,
+            size_t column_size)
+{
+    for_each(
+        execution::par,         // <- No realization in C++17
+        begin(result),
+        end(result),
+        [&first_row, column_size] (vector<int> &row)
+        {
+            row.reserve(column_size);
+            for (size_t column = 0; column < column_size; ++column)
+            {
+                row.push_back(first_row ^ column);
+            }
+            ++first_row;
+        }
+    );
+}
+
+vector<vector<int>> GenerateSingleThread(size_t size)
+{
+    vector<vector<int>> result(size);
+    GenerateSingleThread(result, 0, size);
+    return result;
+}
+
+
+template <typename ContainerOfVectors>
+int64_t SumSingleThread(const ContainerOfVectors& matrix)
+{
+    int64_t sum = 0;
+    for (const auto& row : matrix)
+    {
+        for (auto item : row)
+        {
+            sum += item;
+        }
+    }
+    return sum;
+}
+
+int main()
+{
+    LOG_DURATION("Total");
+    const size_t matrix_size = 15'000;
+    const size_t page_size = 7500;
+
+    vector<vector<int>> matrix_single, matrix_multi;
+    {
+        LOG_DURATION("Single thread generation");
+        matrix_single = GenerateSingleThread(matrix_size);
+    }
+    {
+        LOG_DURATION("Single thread sum");
+        cout << SumSingleThread(matrix_single) << endl;
+    }
+}
