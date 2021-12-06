49cfdfd30281bb033bfb2c9b4f229c5db46eb039
Xelerezex <nikitavalderman@gmail.com>
2021-12-06 20:03:26 +0300

    C++. C++17 STL. chapter-7-strings-io-re. Adding Parts from 01 - 09.

---
+++ C++/С++17-STL-J-Galowicz/chapter-7-strings-io-re/02-del-first-last-spaces/02-del-first-last-spaces-0.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+string trim_whitespace_surrounding(const string &s)
+{
+    const char whitespace[] {" \t\n"};
+    const size_t first (s.find_first_not_of(whitespace));
+    if (string::npos == first) {return {}; }
+    const size_t last (s.find_last_not_of(whitespace));
+    return s.substr(first, (last - first + 1));
+}
+
+int main()
+{
+    string s {"  \t\n string surrounded by ugly"
+              " whitespaces   \t\n    "};
+
+              cout << "{" << s << "}\n";
+              cout << "{"
+                   << trim_whitespace_surrounding(s)
+                   << "}\n";
+}
