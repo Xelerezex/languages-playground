434d0a1467b121f1636266e820929e76e8b80143
Xelerezex <nikitavalderman@gmail.com>
2022-04-18 18:58:24 +0300

    C++. Art of C++. Brown Belt. Week 4. Adding listing 8 from lecture.

---
+++ C++/Art-Of-Development-C++/4-brown-belt/week-4/08-raii-part-1.cpp
@@ -0,0 +1,164 @@
+#include "tools.h"
+
+using namespace std;
+
+class FlightProvider
+{
+    public:
+        using BookingId = int;
+
+        struct BookingData
+        {
+            string city_from;
+            string city_to;
+            string date;
+        };
+
+        BookingId Book (const BookingData& data)
+        {
+            ++counter;
+            if (counter > 1)
+            {
+                throw runtime_error("Overbooking");
+            }
+            cerr << "Flight booking: " << counter << "\n";
+            return counter;
+        }
+
+        void Cancel (const BookingId& id)
+        {
+            --counter;
+            cerr << "Cancel flight: " << id << "\n";
+        }
+
+    private:
+        int counter = 0;
+};
+
+class HotelProvider
+{
+    public:
+        using BookingId = int;
+
+        struct BookingData
+        {
+            string city;
+            string date_from;
+            string date_to;
+        };
+
+        BookingId Book (const BookingData& data)
+        {
+            ++counter;
+            cerr << "Hotel booking: " << counter << "\n";
+            return counter;
+        }
+
+        void Cancel (const BookingId& id)
+        {
+            --counter;
+            cerr << "Cancel hotel: " << id << "\n";
+        }
+
+    private:
+        int counter = 0;
+};
+
+class Trip
+{
+    private:
+        HotelProvider&  hotel_provider;
+        FlightProvider& flight_provider;
+
+    public:
+        vector<HotelProvider::BookingId> hotels;
+        vector<FlightProvider::BookingId> flights;
+
+        Trip(const Trip&) = delete;
+        Trip(Trip&&) = default;
+
+        Trip& operator= (const Trip&) = delete;
+        Trip& operator= (Trip&&) = default;
+
+        Trip(HotelProvider& hp, FlightProvider& fp)
+            : hotel_provider(hp)
+            , flight_provider(fp)
+        {
+        }
+
+        void Cancel()
+        {
+            for (auto& id : hotels)
+            {
+                hotel_provider.Cancel(id);
+            }
+            hotels.clear();
+            for (auto& id : flights)
+            {
+                flight_provider.Cancel(id);
+            }
+            flights.clear();
+        }
+
+        ~Trip()
+        {
+            Cancel();
+        }
+};
+
+class TripManager
+{
+    public:
+        using BookingId = int;
+
+        struct BookingData
+        {
+            string city_from;
+            string city_to;
+            string date_from;
+            string date_to;
+        };
+
+        Trip Book (const BookingData& data)
+        {
+            Trip trip(hotel_provider, flight_provider);
+
+            {
+                FlightProvider::BookingData data;
+                trip.flights.push_back(flight_provider.Book(data));
+            }
+            {
+                HotelProvider::BookingData data;
+                trip.hotels.push_back(hotel_provider.Book(data));
+            }
+            {
+                FlightProvider::BookingData data;
+                trip.flights.push_back(flight_provider.Book(data));
+            }
+
+            return trip;
+        }
+
+        void Cancel (Trip& trip)
+        {
+            trip.Cancel();
+        }
+
+    private:
+        HotelProvider hotel_provider;
+        FlightProvider flight_provider;
+};
+
+int main()
+{
+    try
+    {
+        TripManager tm;
+        auto trip = tm.Book({});
+        tm.Cancel(trip);
+    }
+    catch(...)
+    {
+        cerr << "Exception\n";
+    }
+}
