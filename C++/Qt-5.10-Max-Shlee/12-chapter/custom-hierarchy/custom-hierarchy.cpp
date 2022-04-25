cdd7d3f4b79ffce9519a2e5febb6d543b38fcb01
Xelerezex <nikitavalderman@gmail.com>
2022-04-25 19:09:14 +0300

    Added chapter 12, part II.

---
+++ C++/Qt-5.10-Max-Shlee/12-chapter/custom-hierarchy/custom-hierarchy.cpp
@@ -0,0 +1,20 @@
+#include <QtWidgets>
+#include "intListModel.hpp"
+
+
+// ----------------------------------------------------------------------
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+    IntListModel model(QList<int>() << 123 << 2341 << 32 << 5342 << 723);
+
+    QListView list;
+    list.setModel(&model);
+    list.show();
+
+    QTableView table;
+    table.setModel(&model);
+    table.show();
+
+    return app.exec();
+}
