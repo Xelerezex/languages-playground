461623749c02b176e45cdcbf7715b70a76f6cbf9
Nikita Beletsky <nikitavalderman@gmail.com>
2022-11-09 13:06:55 +0300

    C++. StackOverFlow. Copy & Swap Idiom. Added bad operator=.

--- C++/StackOverFlow-C++/01-copy-and-swap-idiom.cpp
+++ C++/StackOverFlow-C++/01-copy-and-swap-idiom.cpp
@@ -20,11 +20,29 @@ public:
         : m_sSize{other.m_sSize}
         , m_pArray{m_sSize ? new int[m_sSize]() : nullptr}
     {
-        // note that this is non-throwing, because of the data
+        // Note that this is non-throwing, because of the data
         // types being used; more attention to detail with regards
         // to exceptions must be given in a more general case, however
         std::copy(other.m_pArray, other.m_pArray + m_sSize, m_pArray);
         std::cerr << "Called Copy constructor for object: " << this << std::endl;
+    }
+
+    // Assignment operator
+    wrapper& operator= (const wrapper& other)
+    {
+        if (this != &other)
+        {
+            // Delete all old data
+            delete [] m_pArray;
+            m_pArray = nullptr;
+
+            m_sSize = other.m_sSize;
+            m_pArray = m_sSize ? new int[m_sSize] : nullptr;
+            std::copy(other.m_pArray, other.m_pArray + m_sSize, m_pArray);
+        }
+
+        std::cerr << "Called Assignment operator for object: " << this << std::endl;
+        return *this;
     }
 
     // Destructor
@@ -43,4 +61,7 @@ int main()
 {
     wrapper first(7);
     wrapper second(first);
+    wrapper third;
+
+    third = second;
 }
