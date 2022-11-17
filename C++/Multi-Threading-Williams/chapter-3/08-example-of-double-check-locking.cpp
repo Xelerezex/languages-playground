212f3f984640b198cfd0dc0dd6c28ee368fdc5ec
Nikita Beletsky <nikitavalderman@gmail.com>
2022-11-17 11:27:29 +0300

    C++. Williams. Chapter 03. Listing 8. Rewrite example with call_once.

--- C++/Multi-Threading-Williams/chapter-3/08-example-of-double-check-locking.cpp
+++ C++/Multi-Threading-Williams/chapter-3/08-example-of-double-check-locking.cpp
@@ -8,18 +8,16 @@ public:
 };
 
 std::shared_ptr<some_resource> resource_ptr;
-std::mutex resource_mutex;
+std::once_flag resource_flag;
 
-// Here is redundant serialization
+void init_resource()
+{
+    resource_ptr.reset(new some_resource);
+}
+
 void foo()
 {
-    std::unique_lock<std::mutex> lock(resource_mutex);
-
-    if (!resource_ptr)
-    {
-        resource_ptr.reset(new some_resource);
-    }
-    lock.unlock();
+    std::call_once(resource_flag, init_resource);
     resource_ptr->do_something();
 }
 
