62466598ac1d2caef89fdc1b2a641e2b54506505
Nikita Beletsky <nikitavalderman@gmail.com>
2022-11-17 10:55:43 +0300

    C++. Williams. Chapter 03. Listing 8. Added example with bad serialization.

--- C++/Multi-Threading-Williams/chapter-3/08-example-of-double-check-locking.cpp
+++ C++/Multi-Threading-Williams/chapter-3/08-example-of-double-check-locking.cpp
@@ -1,7 +1,29 @@
 #include <memory>
 #include <mutex>
 
+class some_resource
+{
+public:
+    void do_something() {}
+};
+
+std::shared_ptr<some_resource> resource_ptr;
+std::mutex resource_mutex;
+
+// Here is redundant serialization
+void foo()
+{
+    std::unique_lock<std::mutex> lock(resource_mutex);
+
+    if (!resource_ptr)
+    {
+        resource_ptr.reset(new some_resource);
+    }
+    lock.unlock();
+    resource_ptr->do_something();
+}
+
 int main()
 {
-
+    foo();
 }
