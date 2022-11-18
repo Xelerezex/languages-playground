570c3e43d4a4e2cd1138974d6c412fc9f089d8cc
Nikita Beletsky <nikitavalderman@gmail.com>
2022-11-18 10:17:23 +0300

    C++. Williams. Chapter 03. Listing 10. Added example of shared_mutex.

--- C++/Multi-Threading-Williams/chapter-3/10-example-shared-mutex.cpp
+++ C++/Multi-Threading-Williams/chapter-3/10-example-shared-mutex.cpp
@@ -3,6 +3,30 @@
 #include <mutex>
 #include <shared_mutex>
 
+class dns_entry
+{};
+
+class dns_cache
+{
+public:
+    dns_entry find_entry(const std::string &domain)
+    {
+        std::shared_lock<std::shared_mutex> lock(entry_mutex);
+        const std::map<std::string, dns_entry>::const_iterator it = entries.find(domain);
+        return (it == entries.end()) ? dns_entry{} : it->second;
+    }
+
+    void update_or_add_entry(const std::string &domain, const dns_entry &dns_details)
+    {
+        std::lock_guard<std::shared_mutex> lock(entry_mutex);
+        entries[domain] = dns_details;
+    }
+
+private:
+    std::map<std::string, dns_entry> entries;
+    std::shared_mutex                entry_mutex;
+};
+
 int main()
 {
 
