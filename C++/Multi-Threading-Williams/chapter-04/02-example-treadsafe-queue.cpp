73f274242c10b14977e554d5512095271a951640
Nikita Beletsky <nikitavalderman@gmail.com>
2022-11-24 10:48:16 +0300

    C++. Williams. Chapter 04. Listing 2. Added threadsafe_queue interface.

--- C++/Multi-Threading-Williams/chapter-04/02-example-treadsafe-queue.cpp
+++ C++/Multi-Threading-Williams/chapter-04/02-example-treadsafe-queue.cpp
@@ -1,5 +1,24 @@
 #include <memory>
 
+template <typename T>
+class threadsafe_queue
+{
+public:
+        treadsafe_queue();
+        treadsafe_queue(const treadsafe_queue&);
+        treadsafe_queue& operator=(const treadsafe_queue&) = delete;
+
+        void push(T new_value);
+
+        bool try_pop(T& value);
+        std::shared_ptr<T> try_pop();
+
+        void wait_and_pop(T& value);
+        std::shared_ptr<T> wait_and_pop();
+
+        bool empty() const;
+};
+
 int main()
 {
 
